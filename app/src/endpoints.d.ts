8cc049f1aeb8c48926949b80ec8219a9897de28d
sothy_l <sothy_l>
2025-11-19 13:42:56 +0700

    fix

--- app/src/endpoints.d.ts
+++ app/src/endpoints.d.ts
@@ -1,6 +1,6 @@
 /* tslint:disable */
 /* eslint-disable */
-// Generated using typescript-generator version 3.2.1263 on 2025-11-18 08:43:06.
+// Generated using typescript-generator version 3.2.1263 on 2025-11-18 18:56:12.
 
 declare namespace endpoint {
 
